4eb4a6f3c8a936cc7e2a1d305cf5989bf3878deb
olukowade samson <solukowade@gmail.com>
2024-10-16 18:57:16 +0100

    learned how to integrate componenets in react

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import { react,Component } from "react";
+const Footer = () => {
+    return(
+        <>
+        <div>
+            <footer>
+                <p>&copy; Event Planner Organization. All rights reserved.</p>
+            </footer>
+        </div>
+
+        </>
+    )
+}
+export default Footer
